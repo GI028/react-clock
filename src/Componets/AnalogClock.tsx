1952b2c2a92b7790aa1a6a1f6897c19064711788
Rabibisoa Giovanni <187939419+GI028@users.noreply.github.com>
2025-04-07 17:54:03 +0800

    optimizing the component. using useRef to insted of useState to make the component not rendering each time

--- src/Componets/AnalogClock.tsx
+++ src/Componets/AnalogClock.tsx
@@ -1,33 +1,48 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useRef, useState } from "react";
 
-const numbers: number[] = []
+const numbers: number[] = [];
 for (let i = 1; i <= 12; i++) {
-  numbers.push(i)
+  numbers.push(i);
 }
 
 type Time = {
-  hours: number
-  minutes: number
-  seconds: number
+  hours: number;
+  minutes: number;
+  seconds: number;
+};
+function timeAreSames(time1: Time, time2: Time) {
+  return (
+    time1.hours === time2.hours &&
+    time1.minutes === time2.minutes &&
+    time1.seconds === time2.seconds
+  );
 }
 export default function AnalogClock() {
-  const [time, setTime] = useState<Time>({
+  const [_, setState] = useState<boolean>(false);
+  const refresh = () => setState((current)=>!current);
+  const ref = useRef<Time>({
     hours: 12,
     minutes: 0,
     seconds: 0,
-  })
+  });
+  const time = ref.current;
   useEffect(() => {
     const interval = setInterval(() => {
-      const now = new Date()
-      const newTime:Time = {
+      const now = new Date();
+      const newTime: Time = {
         hours: now.getHours(),
         minutes: now.getMinutes(),
         seconds: now.getSeconds(),
+      };
+      const oldTime = ref.current;
+      // console.log(ref.current);
+      if (!timeAreSames(oldTime, newTime)) {
+        ref.current = newTime;
+        refresh();
       }
-      setTime(newTime)
-    }, 100)
-    return () => clearInterval(interval)
-  }, [time.seconds])
+    }, 100);
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div className="analog-clock">
       {numbers.map((number) => (
@@ -39,14 +54,23 @@ export default function AnalogClock() {
           <span>{number}</span>
         </div>
       ))}
-      <div className="minute-hand" style={{transform:`rotate(${getHandAngle(time.minutes, 60, 6)}deg)`}}/>
-      <div className="hour-hand" style={{transform:`rotate(${getHandAngle(time.hours, 12, 30)}deg)`}}/>
-      <div className="second-hand" style={{transform:`rotate(${getHandAngle(time.seconds, 60, 6)}deg)`}}/>
+      <div
+        className="minute-hand"
+        style={{ transform: `rotate(${getHandAngle(time.minutes, 60, 6)}deg)` }}
+      />
+      <div
+        className="hour-hand"
+        style={{ transform: `rotate(${getHandAngle(time.hours, 12, 30)}deg)` }}
+      />
+      <div
+        className="second-hand"
+        style={{ transform: `rotate(${getHandAngle(time.seconds, 60, 6)}deg)` }}
+      />
       <div className="center-hand" />
     </div>
-  )
+  );
 }
 
-function getHandAngle(value:number, maxValue:number, step:number){
-    return (value % maxValue) * step;
+function getHandAngle(value: number, maxValue: number, step: number) {
+  return (value % maxValue) * step;
 }
